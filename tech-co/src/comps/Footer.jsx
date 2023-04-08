1d216d8b23660cebeae53a16811cedf5ca4af903
i7mada249 <i7mada249@outlook.com>
2023-04-08 03:46:05 -0700

    footer

--- tech-co/src/comps/Footer.jsx
+++ tech-co/src/comps/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='footer'>
         <p>All rights reserved</p>
-        <p>designed by <a href=""></a></p>
+        <p>designed by <a href="https://www.figma.com/community/file/1073705173542690146">Armenuhi Ivanyan</a></p>
         <p>developed by <a href="https://github.com/i7mada249">Mohammed Almustafa</a></p>
     </div>
   )
